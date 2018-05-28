1b44844e1c2b6f8e8b4b8c9ecba27e5ae0246e9f
Keylin_D <keylin.05.01@gmail.com>
2018-05-28 12:56:27 -0400

    Inicia ek commit

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    let food   = prompt('Qué deseas  ordenar');
+
+    switch (food) {
+        case 'ceviche':
+            alert('No se encuentra en el menú');
+            prompt('Pida otra orden');
+            break;
+        case 'tacos al pastor':
+            alert('No se encuentra en el menú');
+            prompt('Pida otra orden');
+            break;
+        case 'empanada chilena':
+            alert('No se encuentra en el menú');
+            prompt('Pida otra orden');
+            break;
+        case 'hamburguesa':
+            alert('Buena elección');
+        default:
+            
+            
+    }
+}
